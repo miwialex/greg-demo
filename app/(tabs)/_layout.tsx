d7e4edf3097821d5a95a4e604a6ea0a28cf52b95
Ruben Manrique <rubenmanrique@mac.lan>
2025-02-10 20:50:11 -0600

    initial

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,14 +29,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Map',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Places',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
